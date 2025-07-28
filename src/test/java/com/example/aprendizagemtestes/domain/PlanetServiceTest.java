7ddb2abc3ee05b1856d92729bd248beca8714a72
arthur810FR <arthurfranciscoazevedo@gmail.com>
2025-07-28 16:07:24 -0300

    Testando os cenarios de erro

--- src/test/java/com/example/aprendizagemtestes/domain/PlanetServiceTest.java
+++ src/test/java/com/example/aprendizagemtestes/domain/PlanetServiceTest.java
@@ -7,7 +7,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static com.example.aprendizagemtestes.common.PlanetConstants.PLANET;
+import static com.example.aprendizagemtestes.common.PlanetConstants.INVALID_PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,5 +29,12 @@ public class PlanetServiceTest {
         Planet su = planetService.create(PLANET);
 
         assertThat(su).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException() {
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
     }
 }
