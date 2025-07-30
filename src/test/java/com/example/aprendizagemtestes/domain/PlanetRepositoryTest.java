5572aa433fa8d4fa0546e879eb556e5b0440b977
arthur810FR <arthurfranciscoazevedo@gmail.com>
2025-07-30 12:10:44 -0300

    Testando o cenario de sucesso no repository

---
+++ src/test/java/com/example/aprendizagemtestes/domain/PlanetRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.aprendizagemtestes.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static com.example.aprendizagemtestes.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class PlanetRepositoryTest {
+
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsPlanet() {
+        Planet planet = planetRepository.save(PLANET);
+
+        Planet sut = testEntityManager.find(Planet.class, planet.getId());
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+}
